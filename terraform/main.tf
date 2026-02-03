79dd84ee66d81657abf63298a3c6e43c6eacaa3e
Yogesh Patil <yogeshpatil060920@gmail.com>
2026-02-03 18:37:46 +0000

    Add service account and IAM roles for weather data pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,37 @@ resource "google_bigquery_dataset" "weather_dataset" {
 
   delete_contents_on_destroy = false
 }
+
+
+# -----------------------------
+# Service Account for Pipeline
+# -----------------------------
+resource "google_service_account" "weather_pipeline_sa" {
+  account_id   = "weather-pipeline-sa"
+  display_name = "Weather Data Pipeline Service Account"
+  description  = "Service account used by Airflow weather data pipeline"
+}
+
+# -----------------------------
+# GCS Bucket IAM
+# -----------------------------
+resource "google_storage_bucket_iam_member" "gcs_object_admin" {
+  bucket = google_storage_bucket.raw_weather_bucket.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.weather_pipeline_sa.email}"
+}
+
+# -----------------------------
+# BigQuery Dataset IAM
+# -----------------------------
+resource "google_bigquery_dataset_iam_member" "bq_data_editor" {
+  dataset_id = google_bigquery_dataset.weather_dataset.dataset_id
+  role       = "roles/bigquery.dataEditor"
+  member     = "serviceAccount:${google_service_account.weather_pipeline_sa.email}"
+}
+
+resource "google_bigquery_dataset_iam_member" "bq_job_user" {
+  dataset_id = google_bigquery_dataset.weather_dataset.dataset_id
+  role       = "roles/bigquery.jobUser"
+  member     = "serviceAccount:${google_service_account.weather_pipeline_sa.email}"
+}
